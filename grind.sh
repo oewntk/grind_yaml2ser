2918edb25c0686d0247308b54e8f9e98e26528de
1313ou <1313ou@gmail.com>
2022-01-06 10:24:04 +0100

    scripts

--- grind.sh
+++ grind.sh
@@ -14,9 +14,14 @@ echo "YAML2: ${IN2}" 1>&2;
 
 OUTDIR="$3"
 if [ -z "$3" ]; then
-	OUTDIR=wndb
+	OUTDIR=ser
 fi
 mkdir -p "${OUTDIR}"
 echo "DIR:   "${OUTDIR}"" 1>&2;
 
-java -ea -jar oewn-grind-yaml2ser.jar "${IN}" "${IN2}" "${OUTDIR}"
+OUTSER="$4"
+if [ -z "$4" ]; then
+	OUTSER=oewn.ser
+fi
+
+java -ea -jar oewn-grind-yaml2ser.jar "${IN}" "${IN2}" "${OUTDIR}/${OUTSER}"
