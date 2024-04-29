a5adf421e6928066c4b8e759937893d21262c3c0
1313ou <1313ou@gmail.com>
2024-04-29 19:44:09 +0200

    reformat

--- src/main/java/org/oewntk/grind/yaml2ser/Tracing.kt
+++ src/main/java/org/oewntk/grind/yaml2ser/Tracing.kt
@@ -14,96 +14,97 @@ import java.util.*
  * @author Bernard Bou
  */
 object Tracing {
-	val psInfo: PrintStream = System.out
-	val psErr: PrintStream = System.err
-	private val psHeap: PrintStream = System.out
-	private val psTime: PrintStream = System.out
 
-	private const val TRACE_HEAP: Boolean = false
+    val psInfo: PrintStream = System.out
+    val psErr: PrintStream = System.err
+    private val psHeap: PrintStream = System.out
+    private val psTime: PrintStream = System.out
 
-	private const val TRACE_TIME: Boolean = false
+    private const val TRACE_HEAP: Boolean = false
 
-	fun start(): Long {
-		val startTime = System.currentTimeMillis()
+    private const val TRACE_TIME: Boolean = false
 
-		// heap state
-		if (TRACE_HEAP) {
-			psHeap.println("[Memory]: " + Memory.memoryInfo("before,", Memory.Unit.M))
-			psHeap.println("[Heap]: " + Memory.heapInfo("before,", Memory.Unit.M))
-		}
-		return startTime
-	}
+    fun start(): Long {
+        val startTime = System.currentTimeMillis()
 
-	fun progress(message: String?, startTime: Long) {
-		if (TRACE_TIME) {
-			val endTime = System.currentTimeMillis()
-			psTime.println("[Time]: " + (endTime - startTime) / 1000 + "s")
-		}
+        // heap state
+        if (TRACE_HEAP) {
+            psHeap.println("[Memory]: " + Memory.memoryInfo("before,", Memory.Unit.M))
+            psHeap.println("[Heap]: " + Memory.heapInfo("before,", Memory.Unit.M))
+        }
+        return startTime
+    }
 
-		// heap state
-		if (TRACE_HEAP) {
-			psHeap.println("[Heap]: " + Memory.heapInfo(message, Memory.Unit.M))
-		}
-	}
+    fun progress(message: String?, startTime: Long) {
+        if (TRACE_TIME) {
+            val endTime = System.currentTimeMillis()
+            psTime.println("[Time]: " + (endTime - startTime) / 1000 + "s")
+        }
 
-	object Memory {
+        // heap state
+        if (TRACE_HEAP) {
+            psHeap.println("[Heap]: " + Memory.heapInfo(message, Memory.Unit.M))
+        }
+    }
 
-		private fun formatter(): DecimalFormat {
-			val formatter = NumberFormat.getInstance(Locale.US) as DecimalFormat
-			val symbols = formatter.decimalFormatSymbols
-			symbols.groupingSeparator = ' '
-			formatter.decimalFormatSymbols = symbols
-			return formatter
-		}
+    object Memory {
 
-		fun heapInfo(tag: String?, u: Unit): String {
-			val max = Runtime.getRuntime().maxMemory()
-			val total = Runtime.getRuntime().totalMemory()
-			val free = Runtime.getRuntime().freeMemory()
-			var used = total - free
-			val future = max - total
-			var avail = free + future
+        private fun formatter(): DecimalFormat {
+            val formatter = NumberFormat.getInstance(Locale.US) as DecimalFormat
+            val symbols = formatter.decimalFormatSymbols
+            symbols.groupingSeparator = ' '
+            formatter.decimalFormatSymbols = symbols
+            return formatter
+        }
 
-			used /= u.div
-			avail /= u.div
+        fun heapInfo(tag: String?, u: Unit): String {
+            val max = Runtime.getRuntime().maxMemory()
+            val total = Runtime.getRuntime().totalMemory()
+            val free = Runtime.getRuntime().freeMemory()
+            var used = total - free
+            val future = max - total
+            var avail = free + future
 
-			val formatter = formatter()
-			return String.format(
-				"%s used: %s%s maxfree: %s%s",  //
-				tag,  //
-				formatter.format(used), u,  //
-				formatter.format(avail), u //
-			)
-		}
+            used /= u.div
+            avail /= u.div
 
-		fun memoryInfo(tag: String?, u: Unit): String {
-			var max = Runtime.getRuntime().maxMemory()
-			var total = Runtime.getRuntime().totalMemory()
-			var free = Runtime.getRuntime().freeMemory()
-			var used = total - free
-			val future = max - total
-			var avail = free + future
+            val formatter = formatter()
+            return String.format(
+                "%s used: %s%s maxfree: %s%s",  //
+                tag,  //
+                formatter.format(used), u,  //
+                formatter.format(avail), u //
+            )
+        }
 
-			max /= u.div // This will return Long.MAX_VALUE if there is no preset limit
-			total /= u.div
-			free /= u.div
-			used /= u.div
-			avail /= u.div
+        fun memoryInfo(tag: String?, u: Unit): String {
+            var max = Runtime.getRuntime().maxMemory()
+            var total = Runtime.getRuntime().totalMemory()
+            var free = Runtime.getRuntime().freeMemory()
+            var used = total - free
+            val future = max - total
+            var avail = free + future
 
-			val formatter = formatter()
-			return String.format(
-				"%s max=%15s%s total=%10s%s used=%15s%s free=%15s%s avail=%15s%s",  //
-				tag,  //
-				formatter.format(max), u,  //
-				formatter.format(total), u,  //
-				formatter.format(used), u,  //
-				formatter.format(free), u,  //
-				formatter.format(avail), u
-			)
-		}
+            max /= u.div // This will return Long.MAX_VALUE if there is no preset limit
+            total /= u.div
+            free /= u.div
+            used /= u.div
+            avail /= u.div
 
-		enum class Unit(val div: Long) {
-			U(1), K(1024 * U.div), M(1024 * K.div), G(1024 * M.div)
-		}
-	}
+            val formatter = formatter()
+            return String.format(
+                "%s max=%15s%s total=%10s%s used=%15s%s free=%15s%s avail=%15s%s",  //
+                tag,  //
+                formatter.format(max), u,  //
+                formatter.format(total), u,  //
+                formatter.format(used), u,  //
+                formatter.format(free), u,  //
+                formatter.format(avail), u
+            )
+        }
+
+        enum class Unit(val div: Long) {
+            U(1), K(1024 * U.div), M(1024 * K.div), G(1024 * M.div)
+        }
+    }
 }
