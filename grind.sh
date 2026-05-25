f8f5ee56d67740d0aa7e0ab5d3cb8d4c36e7991d
1313ou <1313ou@gmail.com>
2026-05-25 09:51:04 +0200

    bumo to v 2.4.0

--- grind.sh
+++ grind.sh
@@ -35,5 +35,5 @@ if [ -z "$5" ]; then
 	OUTSERINFO=oewn.ser.info
 fi
 
-jar=target/yaml2ser-2.3.2-uber.jar
+jar=target/yaml2ser-2.4.0-uber.jar
 java -ea -jar "${jar}" "${IN}" "${IN2}" "${OUTDIR}/${OUTSER}" "${OUTDIR}/${OUTSERINFO}"
