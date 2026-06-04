f1288c6cc7e57b1f8b7433958a26b3b196dcca6e
1313ou <1313ou@gmail.com>
2026-06-04 15:27:32 +0200

    add cli

--- src/main/java/org/oewntk/grind/yaml2ser/Grind.kt
+++ src/main/java/org/oewntk/grind/yaml2ser/Grind.kt
@@ -3,6 +3,9 @@
  */
 package org.oewntk.grind.yaml2ser
 
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
 import org.oewntk.grind.yaml2ser.Tracing.progress
 import org.oewntk.grind.yaml2ser.Tracing.start
 import org.oewntk.model.ModelInfo
@@ -28,27 +31,37 @@ object Grind {
      */
     @JvmStatic
     fun main(args: Array<String>) {
-        var iArg = 0
+        val parser = ArgParser("yaml2ser")
+
+        // Options (start with - or --)
+        // @formatter:off
+        val in1 by parser.argument(    ArgType.String,                                              description = "Input dir or file")
+        val in2 by parser.argument(    ArgType.String,                                              description = "Extra input dir or file")
+        val out by parser.argument(    ArgType.String,                                              description = "Output dir or file")
+        val out2 by parser.option(     ArgType.String,   shortName = "o2", fullName = "out2",       description = "Extra output dir or file")        .default("")
+        val verbose by parser.option(  ArgType.Boolean,  shortName = "v",  fullName = "verbose",    description = "Verbose output")                  .default(false)
+
+        val traceTime by parser.option(ArgType.Boolean,  shortName = "tt", fullName = "trace:time", description = "trace time")                      .default(false)
+        val traceHeap by parser.option(ArgType.Boolean,  shortName = "th", fullName = "trace:heap", description = "trace heap")                      .default(false)
+        // @formatter:on
+        parser.parse(args)
 
         // Tracing
+        Tracing.traceTime = traceTime
+        Tracing.traceHeap = traceHeap
+
         val startTime = start()
 
-        // verbose
-        var verbose = false
-        if (args[iArg] == "-verbose") {
-            verbose = true
-            iArg++
-        }
         // Input
-        val inDir = File(args[iArg])
+        val inDir = File(in1)
         Tracing.psInfo.println("[Input] " + inDir.absolutePath)
 
         // Input2
-        val inDir2 = File(args[iArg + 1])
+        val inDir2 = File(in2)
         Tracing.psInfo.println("[Input2] " + inDir2.absolutePath)
 
         // Output
-        val outFile = File(args[iArg + 2])
+        val outFile = File(out)
         val outDir = outFile.parentFile
         if (!outDir.exists()) {
             outDir.mkdirs()
@@ -61,13 +74,11 @@ object Grind {
         // Supply model
         progress("before model is supplied,", startTime)
         val model = Factory(inDir, inDir2, verbose = verbose).get()!!
-
-        //Tracing.psInfo.printf("[Model] %s%n%s%n%n", Arrays.toString(model.getSources()), model.info());
         progress("after model is supplied,", startTime)
 
         // Consume model
         progress("before model is consumed,", startTime)
-        ModelConsumer(outFile).accept(model)
+        ModelConsumer(outFile, verbose = verbose).accept(model)
         progress("after model is consumed,", startTime)
 
         // End
@@ -78,6 +89,6 @@ object Grind {
         val modelCounts = ModelInfo.counts(model)
         val modelInfo2 = "$modelInfo\n$modelCounts"
         Tracing.psInfo.println(modelInfo2)
-        File(if (args.size == 4) args[iArg + 3] else "oewn.ser.info").writeText(modelInfo2)
+        File(out2.ifEmpty { "oewn.ser.info" }).writeText(modelInfo2)
     }
 }
